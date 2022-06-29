7743ebbce89a944c2bb8a0356f6bcb7d61c03d6a
mjjn8909 <64269019+mjjn8909@users.noreply.github.com>
2022-06-29 10:22:07 +0530

    Update 09_minimize_the_height.cpp
    Loop Initialization error solved.
    Now it works on gfg practice.

--- 01_array/09_minimize_the_height.cpp
+++ 01_array/09_minimize_the_height.cpp
@@ -13,7 +13,7 @@ int getMinDiff(int arr[], int n, int k) {
     sort(arr, arr + n);
     int ans = arr[n - 1] - arr[0];
 
-    for (int i = 0;i < n;i++) {
+    for (int i = 1;i < n;i++) {
         if (arr[i] < k) continue;     // to avoid negative height in curr_min
         int curr_min = min(arr[0] + k, arr[i] - k);
         int curr_max = max(arr[n - 1] - k, arr[i - 1] + k);
